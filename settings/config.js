2a20f0594f721176cf5dc45cca4db5d3004f7ad4
HanZu0220 <104676099+HanZu0220@users.noreply.github.com>
2022-07-10 12:45:23 +0800

    Delete config.js

--- settings/config.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-    TOKEN: process.env.TOKEN || "OTQ5NDMxNzQzNDcwMDAyMjA2.YiKRNw.xSK1VI6FzogdUUGHCCrzfsOflpM",  // your bot token
-    PREFIX: process.env.PREFIX || "$", //<= default is #  // bot prefix
-    OWNER_ID: process.env.OWNER_ID || "526366736908419092", //your client id
-}
